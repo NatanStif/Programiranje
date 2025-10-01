b1cfd62a04db49e9200b772cbcb186a0cd930354
NatanStif <natan.stifanic@gmail.com>
2025-10-01 09:33:18 +0200

    Update main.py
    Dodana funkcionalnost čišćenja teksta i razdvajanja teksta u riječi.

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-#Funkcija za učitavanje teksta iz datoteke
+#Funkcija za učitavanje teksta iz datoteke 
 def ucitaj_tekst(filepath):
     try:
-        #Ovdje ide logika za čitanje datoteke
+        # Kod za otvaranje datoteke ide ovdje
         with open (filepath, 'r', encoding='utf-8') as file:
             sadrzaj = file.read()
         return sadrzaj
     except FileNotFoundError:
-        print(f"Datoteka '{filepath}' nije pronađena.")
-        return None
-
-#Funkcija za pročišćavanje teksta
+        print(f"Greška: Datoteka na putanji '{filepath}' nije pronađena")
+        #Vratit ćemo 'ništa' ako datoteka ne postoji
+        return None 
+      
+# Funkcija za proišćavanje teksta 
 def ocisti_tekst(tekst):
-    #Kod za pročišćavaje teksta
+    #Kod za pročišćavanje teksta ide ovdje
     tekst = tekst.lower()
-    return tekst
-
+    interpunkcija = ['.', ',', '!', '?', ':', ';', '"', "'", '(', ')']
+    for znak in interpunkcija:
+        tekst = tekst.replace(znak, '')
+    lista_rijeci = tekst.split()
+    return lista_rijeci
+       
 if __name__=="__main__":
     filepath = "tekst.txt"
     print(f"Učitavam tekst iz datoteke: {filepath}")
     ucitani_tekst = ucitaj_tekst(filepath)
-    if(ucitani_tekst):
-        print("Učitani tekst je: ")
+    if ucitani_tekst:
+        print("Učitani tekst je:")
         print(ucitani_tekst)
     else:
-        print("Greška pri učitavanju datoteke. ")
+        print("Greška pri učitavanju datoteke.")
     ucitani_tekst = ocisti_tekst(ucitani_tekst)
     if ucitani_tekst:
-        print("Očišćeni tekst je: ")
+        print("Očišćeni tekst je:")
         print(ucitani_tekst)
     else:
-        print("Greška pri očišćavanju teksta. ")        
+        print("Greška pri očišćavanju teksta.")     
 
